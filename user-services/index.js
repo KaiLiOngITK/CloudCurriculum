4b12f205496e8bb0e2b564c7641cdb795d620a76
Kai Li Ong <kai.ong@itk-engineering.de>
2023-05-28 22:21:14 +0200

    [Add] added the HTTP methods

--- user-services/index.js
+++ user-services/index.js
@@ -18,29 +18,76 @@ app.get('/', function (req, res) {
 });
 
 app.post('/users', function (req, res) {
-    
-    
 
-    if(!req.body.id ||
+    if (!req.body.id ||
         !req.body.name ||
-        !req.body.email){
-            res.status(400);
-            res.json({message: "Bad Request"});
-        }else{
-            var user = req.body;
-            users.push({
-                id: req.body.id,
-                name: req.body.name,
-                email: req.body.email
-            });
-            res.json({message: "New user created.", location: "/users/" + req.body.id})
-            res.status(201);
-            console.log(user);
-        }
+        !req.body.email) {
+        res.status(400);
+        res.json({ message: "Bad Request" });
+    } else {
+        var user = req.body;
+        users.push({
+            id: req.body.id,
+            name: req.body.name,
+            email: req.body.email
+        });
+        // res.json({message: "New user created.", location: "/users/" + req.body.id})
+        // res.sendFile(__dirname + '/user-list.html');
+        res.status(201);
+        console.log(user);
+        // res.send('New user has been added into database');
+    }
 });
 
-app.get('/users', function (req, res){
+app.get('/users', function (req, res) {
+    // res.sendFile(__dirname + '/user-list.html');
     res.json(users);
+});
+
+app.get('/users/:id', function (req, res) {
+    const id = req.params.id;
+
+    // Searching users for the id
+    for (let user of users) {
+        if (user.id === id) {
+            res.json(user);
+            return;
+        }
+    }
+
+    // Sending 404 when user not found
+    res.status(404).send('User not found');
+});
+
+app.delete('/users/:id', function (req, res) {
+    // Reading id from the URL
+    const id = req.params.id;
+
+    // Remove item from the users array
+    users = user.filter(i => {
+        if (i.id !== id) {
+            return true;
+        }
+        return false;
+    });
+
+    res.send('User is deleted');
+});
+
+app.post('/users/:id', function (req, res) {
+    // Reading id from the URL
+    const id = req.params.id;
+    const newUser = req.user;
+
+    // Remove item from the users array
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i]
+        if (user.id == id) {
+            users[i] = newUser;
+        }
+    }
+
+    res.send('User is edited');
 });
 
 var server = app.listen(port, function () {
