08be50dd6338d9c02e16eae448afbe390960ba6b
Kai Li Ong (Kylie) <115071716+KaiLiOngITK@users.noreply.github.com>
2023-06-02 00:06:27 +0200

    [Update] Configure with dockerfile

--- user-services/index.js
+++ user-services/index.js
@@ -1,76 +1,61 @@
 const express = require('express');
 const es6Renderer = require('express-es6-template-engine');
-const exphbs = require('express-handlebars');
-const bodyParser = require("body-parser");
-const cors = require('cors');
+// const bodyParser = require("body-parser");
 
 var app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.engine('html', es6Renderer);
 app.set('views', 'views');
 app.set('view engine', 'html');
 
-// Where we will keep the users data
+app.use(express.json());
+
+// For keeping the users data
 let users = [];
 
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.render('index', { locals: { title: 'User Management Service' } });
 });
 
+// GET /users - Retrieve all products
+app.get('/users', function (req, res) {
+    res.json(users);
+});
+
+// POST /users - Create a new user
 app.post('/users', function (req, res) {
 
     if (!req.body.id ||
         !req.body.name ||
         !req.body.email) {
-        res.status(400);
-        res.json({ message: "Bad Request" });
+        res.status(400).res.json({ message: "Bad Request" });
     } else {
         var user = req.body;
-        users.push({
-            id: req.body.id,
-            name: req.body.name,
-            email: req.body.email
-        });
-        // res.json({message: "New user created.", location: "/users/" + req.body.id})
-        res.json(users);
-        res.status(201);
-        console.log(user);
-
-        // res.render(__dirname + '/views/user-list.html', { token: users });
-        // res.sendFile(__dirname + '/views/user-list.html');
-        // res.send('New user has been added into database');
+        users.push(user);
+        res.status(201).json(user);
     }
 });
 
-app.get('/users', function (req, res) {
-    // res.sendFile(__dirname + '/views/user-list.html');
-    res.json(users);
-});
-
+// GET /users/:id - Retrieve user by ID
 app.get('/users/:id', function (req, res) {
-    const id = req.params.id;
+    const id = +req.params.id;
+    const user = users.find((user) => user.id === id);
 
-    // Searching users for the id
-    for (let user of users) {
-        if (user.id === id) {
-            res.json(user);
-            return;
-        }
+    if (user) {
+        res.json(user);
+    } else {
+        // Sending 404 when user not found
+        res.status(404).json({ error: 'User not found' });
     }
-
-    // Sending 404 when user not found
-    res.status(404).send('Status 404: User not found');
 });
 
 app.delete('/users/:id', function (req, res) {
     // Reading id from the URL
-    const id = req.params.id;
+    const id = +req.params.id;
 
     // Remove item from the users array
     users = users.filter(i => {
@@ -80,21 +65,22 @@ app.delete('/users/:id', function (req, res) {
         return false;
     });
 
-    res.status(200).send('Status 200: User is deleted');
+    res.status(200).json(users);
 });
 
 app.put('/users/:id', function (req, res, next) {
-    const item = users.find(item => item.id === req.params.id);
+    const id = +req.params.id;
+    const item = users.find(item => item.id === id);
     var user = req.body;
 
-    if(item){
+    if (item) {
         item.id = user.id;
         item.name = user.name;
         item.email = user.email;
     }
     res.json(users);
     // res.status(200).send('User is edited. \n');
-    
+
 });
 
 var server = app.listen(port, function () {
