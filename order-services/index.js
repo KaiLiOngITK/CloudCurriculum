382efe8a4228da20d73e32903167c9ee935f92cc
Kai Li Ong (Kylie) <115071716+KaiLiOngITK@users.noreply.github.com>
2023-06-04 19:59:26 +0200

    modified for order services

--- order-services/index.js
+++ order-services/index.js
@@ -1,46 +1,27 @@
 var express = require('express');
-const es6Renderer = require('express-es6-template-engine');
-const exphbs = require('express-handlebars');
-var bodyParser = require("body-parser");
-var cors = require('cors');
 
 var app = express();
-const port = process.env.PORT || 5000;
-
-app.engine('html', es6Renderer);
-app.set('views', 'views');
-app.set('view engine', 'html');
+const port = process.env.PORT || 8082;
 
 // Where we will keep the orders data
 let orders = [];
 
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', function (req, res) {
-    res.render('index', { locals: { title: 'Order Service' } });
+    // res.render('index', { locals: { title: 'Order Service' } });
 });
 
 app.post('/orders', function (req, res) {
     
     if (!req.body.id ||
-        !req.body.userId ||
-        !req.body.productIds) {
-        res.status(400);
-        res.json({ message: "Bad Request" });
+        !req.body.name ||
+        !req.body.description) {
+        res.status(400).res.json({ message: "Bad Request" });
     } else {
         var order = req.body;
-        orders.push({
-            id: req.body.id,
-            userId: req.body.userId,
-            productIds: req.body.productIds
-        });
-        
-        res.json(orders);
-        res.status(201);
-        console.log(order);
+        orders.push(order);
+        res.status(201).json(order);
     }
 });
 
@@ -49,28 +30,26 @@ app.get('/orders', function (req, res) {
 });
 
 app.get('/orders/:id', function (req, res) {
-    const id = req.params.id;
+    const id = +req.params.id;
+    const order = orders.find((order) => order.id === id);
 
-    // Searching orders for the id
-    for (let order of orders) {
-        if (order.id === id) {
-            res.json(order);
-            return;
-        }
+    if (order) {
+        res.json(order);
+    } else {
+        // Sending 404 when user not found
+        res.status(404).json({ error: 'Order not found' });
     }
-
-    // Sending 404 when order not found
-    res.status(404).send('Status 404: Order not found');
 });
 
 app.put('/orders/:id', function (req, res, next) {
-    const item = orders.find(item => item.id === req.params.id);
+    const id = +req.params.id;
+    const item = orders.find(item => item.id === id);
     var order = req.body;
 
-    if(item){
+    if (item) {
         item.id = order.id;
-        item.userId = order.userId;
-        item.productIds = order.productIds;
+        item.name = order.name;
+        item.email = order.email;
     }
     res.json(orders);    
 });
