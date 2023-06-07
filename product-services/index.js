5958337d28239194556bce5917736805905dba2d
Kai Li Ong <kai.ong@itk-engineering.de>
2023-06-08 00:11:17 +0200

    fixed product-services with mongodb

--- product-services/index.js
+++ product-services/index.js
@@ -1,49 +1,145 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
 
-const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-// // parse requests of content-type - application/json
-// app.use(express.json());
-
-// // parse requests of content-type - application/x-www-form-urlencoded
-// app.use(express.urlencoded({ extended: true }));
+var app = express();
+const portAPI = 8081;
 
 app.use(express.json());
 
-const db = require("./app/models");
+// MongoDB connection URI
+const host = process.env.MONGODB_HOST;
 
-console.log(db.url);
+// MongoDB port
+const port = process.env.MONGODB_PORT;
 
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+// MongoDB database name
+const dbName = 'cloudcurriculum';
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to product catalog service." });
+// MongoDB collection name
+const collectionName = 'products';
+
+// MongoDB connection URI
+const uri = `mongodb://${host}:${port}/${dbName}`;
+
+// GET /products - Retrieve all products
+app.get('/products', async function (req, res) {
+  try{
+    const client = new MongoClient(uri);
+    await client.connect();
+    const collection = client.db(dbName).collection(collectionName);
+    const products = await collection.find().toArray();
+    res.json(products);
+  }catch(error){
+    console.error('Error in retrieving products', error);
+    res.status(500).json({error:'Failed to retrieve products'});
+  }
 });
 
-require("./app/routes/product.routes")(app);
+// POST /products - Create a new product
+app.post('/products', async function (req, res) {
+  var product = req.body;
+  try{
+    const client = new MongoClient(uri);
+    await client.connect();
+    const collection = client.db(dbName).collection(collectionName);
+    await collection.insertOne(product);
+    res.status(201).json(product);
+  }catch(error){
+    console.error('Error in creating products', error);
+    res.status(500).json({error:'Failed to create products'});
+  }
+  // res.status(201).json(product);
+});
 
-// set port, listen for requests
-const PORT = process.env.NODE_DOCKER_PORT || 8081;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+// GET /products/:id - Retrieve product by ID
+app.get('/products/:id', async function (req, res) {
+  try {
+    const id = new ObjectId(+req.params.id);
+    const client = new MongoClient(uri);
+    await client.connect();
+    const collection = client.db(dbName).collection(collectionName);
+    const product = await collection.findOne({ _id: id });
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
+  } catch (error) {
+    console.error('Error retrieving product', error);
+    res.status(500).json({ error: 'Failed to retrieve product' });
+  }
+  // const id = +req.params.id;
+  // const product = products.find((product) => product.id === id);
+
+  // if (product) {
+  //   res.json(product);
+  // } else {
+  //   // Sending 404 when product not found
+  //   res.status(404).json({ error: 'product not found' });
+  // }
+});
+
+app.delete('/products/:id', async function (req, res) {
+  try {
+    const id = new ObjectId(req.params.id);
+    const client = new MongoClient(uri);
+    await client.connect();
+    const collection = client.db(dbName).collection(collectionName);
+    const result = await collection.deleteOne({ _id: id });
+    if (result.deletedCount === 1) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting product', error);
+    res.status(500).json({ error: 'Failed to delete product' });
+  }
+  // // Reading id from the URL
+  // const id = +req.params.id;
+
+  // // Remove item from the products array
+  // products = products.filter(i => {
+  //   if (i.id !== id) {
+  //     return true;
+  //   }
+  //   return false;
+  // });
+
+  // res.status(200).json(products);
+});
+
+app.put('/products/:id', async function (req, res, next) {
+  try {
+    const id = new ObjectId(+req.params.id);
+    const updatedProduct = req.body;
+    const client = new MongoClient(uri);
+    await client.connect();
+    const collection = client.db(dbName).collection(collectionName);
+    const result = await collection.updateOne({ _id: id }, { $set: updatedProduct });
+    if (result.modifiedCount === 1) {
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
+  } catch (error) {
+    console.error('Error updating product', error);
+    res.status(500).json({ error: 'Failed to update product' });
+  }
+  // const id = +req.params.id;
+  // const item = products.find(item => item.id === id);
+  // var product = req.body;
+
+  // if (item) {
+  //   item.id = product.id;
+  //   item.name = product.name;
+  //   item.email = product.email;
+  // }
+  // res.json(products);
+  // // res.status(200).send('product is edited. \n');
+
+});
+
+var server = app.listen(portAPI, function () {
+  console.log('Node server is running..');
 });
--- user-services/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id serial PRIMARY KEY,
-    name VARCHAR(50),
-    email VARCHAR(50)
-);
